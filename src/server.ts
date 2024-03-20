88f7e5b488b2359c4fc2f3a12f2fcba6878fc86c
vandodev <vandoaparecido@hotmail.com>
2024-03-20 08:39:23 -0300

    :link: Tratamento de erros

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import fastify from "fastify"
 import { z } from "zod"
 import { sql } from "./lib/postgres"
 
+import postgres from "postgres"
+
 const app = fastify()
 
 app.post("/links", async (request, reply) => {
@@ -13,13 +15,25 @@ app.post("/links", async (request, reply) => {
 
     const { code, url } = requestShema.parse(request.body)
 
-    const result = await sql/*sql*/  `
-      INSERT INTO short_links (code, original_url)
-      VALUES (${code}, ${url})
-      RETURNING id
-    `
-    const link = result[0]
-    return reply.status(201).send({ shortLinkId: link.id })
+    try {
+        const result = await sql/*sql*/  `
+          INSERT INTO short_links (code, original_url)
+          VALUES (${code}, ${url})
+          RETURNING id
+        `
+        const link = result[0]
+        return reply.status(201).send({ shortLinkId: link.id })        
+    } catch (err) {
+        if (err instanceof postgres.PostgresError) {
+            if (err.code === "23505") {
+              return reply.status(400).send({ message: "Duplicated code!" })
+            }
+        }
+
+        console.error(err)
+        return reply.status(500).send({ message: "Internal error." })
+    }
+
 })
 
 app.listen({
