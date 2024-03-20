faf1b3d70bacbe9791497f4c8c411f9945d29490
vandodev <vandoaparecido@hotmail.com>
2024-03-20 08:16:07 -0300

    :link: Validando url com zod

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import fastify from "fastify"
+import { z } from "zod"
 
 const app = fastify()
 
-app.get('/teste', () => {
-    return "Hello world"
+app.post("/links", async (request) => {
+    // const {code, url} = z.object({
+    //     code: z.string().min(3),
+    //     url: z.string().url()
+    // }).parse(request.body)
+
+    const requestShema = z.object({
+        code: z.string().min(3),
+        url: z.string().url()
+      })
+      const { code, url } = requestShema.parse(request.body)
 })
 
 app.listen({
