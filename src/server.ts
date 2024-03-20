dc2e1d859d7378534c10e297c40a767ecf9d614f
vandodev <vandoaparecido@hotmail.com>
2024-03-20 08:29:20 -0300

    :link: Cadastrando link

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,25 @@
 import fastify from "fastify"
 import { z } from "zod"
+import { sql } from "./lib/postgres"
 
 const app = fastify()
 
-app.post("/links", async (request) => {
-    // const {code, url} = z.object({
-    //     code: z.string().min(3),
-    //     url: z.string().url()
-    // }).parse(request.body)
-
+app.post("/links", async (request, reply) => {
+   
     const requestShema = z.object({
         code: z.string().min(3),
         url: z.string().url()
-      })
-      const { code, url } = requestShema.parse(request.body)
+    })
+
+    const { code, url } = requestShema.parse(request.body)
+
+    const result = await sql/*sql*/  `
+      INSERT INTO short_links (code, original_url)
+      VALUES (${code}, ${url})
+      RETURNING id
+    `
+    const link = result[0]
+    return reply.status(201).send({ shortLinkId: link.id })
 })
 
 app.listen({
